4dec14080c5fc3407e8f6eece3dbf4c4835bbd42
Dulat <dulatsagimbayev1@gmail.com>
2020-11-29 15:29:23 +0600

    refactoring to common interface. closes #11.

--- pet-clinic-data/src/main/java/springframework/petclinic/services/VeterinarianService.java
+++ pet-clinic-data/src/main/java/springframework/petclinic/services/VeterinarianService.java
@@ -2,12 +2,6 @@ package springframework.petclinic.services;
 
 import springframework.petclinic.model.Veterinarian;
 
-import java.util.Set;
+public interface VeterinarianService  extends CrudService<Veterinarian, Long>{
 
-public interface VeterinarianService {
-    Veterinarian findById(Long id);
-
-    Veterinarian save (Veterinarian veterinarian);
-
-    Set<Veterinarian> findAll();
 }
